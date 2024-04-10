dcd526c334d660cf0ff677f6d7b48d8550031790
Miloua Mokhtar <80211751+Miloua91@users.noreply.github.com>
2024-04-10 23:03:54 +0100

    added audio component

---
+++ src/components/Waves.tsx
@@ -0,0 +1,68 @@
+import { PauseCircleIcon, PlayCircleIcon } from "@heroicons/react/24/solid";
+import { useEffect, useRef, useState } from "react";
+import WaveSurfer from "wavesurfer.js";
+
+interface prop {
+  audio: string;
+}
+
+export default function Wave(props: prop) {
+  const wavesurferRef = useRef<WaveSurfer | null>(null); // Specify the type of wavesurferRef
+  const [playPause, setPlayPause] = useState(false);
+
+  useEffect(() => {
+    if (!wavesurferRef.current) {
+      wavesurferRef.current = WaveSurfer.create({
+        container: "#waveform",
+        height: 80,
+        waveColor: "#333",
+        progressColor: "#333",
+        url: `/${props.audio}`,
+        barHeight: 1.2,
+        barGap: 2,
+        barWidth: 3,
+      });
+
+      wavesurferRef.current.on("interaction", () => {
+        wavesurferRef.current?.play();
+      });
+    }
+
+    return () => {
+      if (wavesurferRef.current) {
+        wavesurferRef.current.destroy();
+      }
+    };
+  }, [wavesurferRef]);
+
+  const togglePlay = () => {
+    setPlayPause(true);
+    if (wavesurferRef.current) {
+      wavesurferRef.current.playPause();
+    }
+  };
+
+  const togglePause = () => {
+    setPlayPause(false);
+    if (wavesurferRef.current) {
+      wavesurferRef.current.pause();
+    }
+  };
+
+  return (
+    <div className="mb-4 w-full h-20 bg-gray-100 rounded-xl">
+      <div className="flex items-center">
+        <div className="w-10 h-10 py-5 ml-5 mr-2">
+          {!playPause ? (
+            <PlayCircleIcon onClick={togglePlay} />
+          ) : (
+            <PauseCircleIcon onClick={togglePause} />
+          )}
+        </div>
+        <div className="w-full h-10">
+          <div id="waveform" />
+        </div>
+      </div>
+    </div>
+  );
+}
