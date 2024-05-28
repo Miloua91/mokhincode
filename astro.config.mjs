2ac4b53a6708c1307ced601b300df9695cff32ef
Miloua Mokhtar <miloua23@gmail.com>
2024-05-28 18:02:18 +0100

    config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import tunnel from "astro-tunnel";
-//import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), tunnel(), react(), mdx()]
-  //	output: 'server',
-  //	adapter: vercel({
-  //	webAnalytics: {
-  //	enabled: true,
-  //	},
-  //	}),
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    tunnel(),
+    react(),
+    mdx(),
+  ],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
